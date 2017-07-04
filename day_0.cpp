7d6d0d213148386f793a4ff173b0ece7c1f829b0
yawenChen <elfairyhyuk@gmail.com>
2017-07-04 14:08:04 +0800

    change the stopping criterion

--- day_0.cpp
+++ day_0.cpp
@@ -11,7 +11,7 @@ using namespace std;
 void day::processData(ifstream &ifs, ofstream &ofs){
 	string line,value;
 	regex_t reg;
-	regcomp(&reg,"[a-z]+",REG_EXTENDED);//pattern for factory
+	regcomp(&reg,"[a-z]+",REG_EXTENDED);//pattern for categorical data
 	regmatch_t pmatch[1];
 	const size_t nmatch=1;
 	long int count=0;
@@ -21,18 +21,19 @@ void day::processData(ifstream &ifs, ofstream &ofs){
 		//if(countt>3)
 			//break;
 		cout<<countt++<<endl;
-		int i=1;
+		int i=1;//id of feature
+		int ind=0;
 		//label
 		int label=line.find_first_of("\t");
 		value=line.substr(0,label);
 		ofs<<value;
-
+		
 		line=line.substr(label+1,line.length());
-		while(i<40){
-			int ind=line.find_first_of("\t");
-			string value=line.substr(0,ind);
+		while(ind!=-1){
+			ind=line.find_first_of("\t");//tab split each feature
+			string value=line.substr(0,ind);//substr(0,length())==substr(0,-1)
 			line=line.substr(ind+1,line.length());
-
+            //categorical data
 			if(regexec(&reg,value.c_str(),nmatch,pmatch,0)==0){
 				if(cate.find(value)==cate.end()){					
 					cate.insert(make_pair(value,count));
@@ -44,10 +45,8 @@ void day::processData(ifstream &ifs, ofstream &ofs){
 						count++;
 					}
 				}
-				//ofs<<" "<<i<<"::"<<cate[value];
-			//else if(value=="")
-				//ofs<<" "<<i<<"::"<<" ";
-		
+		    //numerical data
+			//skip missing data
 			else if(value!=""){
 				ofs<<" "<<i<<":"<<value;
 			}
