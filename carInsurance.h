0f46f73ed95e6a1c9d0477ecba6fa1ac8dd75f93
yawenChen <elfairyhyuk@gmail.com>
2017-07-16 03:44:50 +0800

    save dataset day_0 in four files

--- carInsurance.h
+++ carInsurance.h
@@ -5,6 +5,7 @@ class carInsurance:public convertData{
 public:
 	carInsurance(){}
     virtual ~carInsurance(){}
+protected:
 	virtual void getFirstLine(ifstream &ifs, ofstream &ofs);
 	virtual void processData(ifstream &ifs, ofstream &ofs);
 };
---
+++ common.h
@@ -0,0 +1,10 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <fstream>
+#include <cstring>
+using namespace std;
+void checkFile(ofstream &ofs, string name);
+void resetFStream(ofstream &ofs);
+
+#endif
