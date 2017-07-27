860db27d5f3b7e2a9683b003a3e4a980e1e59ab7
Zeyi Wen <wenzeyi@gmail.com>
2017-07-27 14:03:18 +0800

    improve run

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #./convert_main ./../dataset/3d_spatial_network.txt lib_3d_spatial_network.txt
-#./convert_main ../dataset/train_set.csv train_set.txt
-./convert_main ../dataset/day_0 day_0.txt
+./convert_main ../dataset/train_set.csv train_set.txt
+#./convert_main ../dataset/day_0 day_0.txt
